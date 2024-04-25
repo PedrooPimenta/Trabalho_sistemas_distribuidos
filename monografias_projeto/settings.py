6ea528a84afc707e0ccd71a4b939bc84ad9f9edc
PedrooPimenta <pedro.pimenta@ufvjm.edu.br>
2024-04-25 11:02:09 -0300

    f

--- monografias_projeto/settings.py
+++ monografias_projeto/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'monografias_projeto.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'projetodjango',
+        'USER': 'root',
+        'PASSWORD':'adm',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
